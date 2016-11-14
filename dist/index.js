8e205e8948135aa39b9451d5c0c2b164735926d3
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-11-14 21:55:22 +0100

    0.1.0

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,30 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _hoistNonReactStatics = require('hoist-non-react-statics');
+
+var _hoistNonReactStatics2 = _interopRequireDefault(_hoistNonReactStatics);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getDisplayName = function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+};
+
+var composeNames = function composeNames(enhancerName, componentName) {
+    return enhancerName ? enhancerName + '(' + componentName + ')' : componentName;
+};
+
+var hoc = function hoc(enhancer, enhancerName, customStatics) {
+    return function (Component) {
+        var Enhanced = (0, _hoistNonReactStatics2.default)(enhancer(Component), Component, customStatics);
+        Enhanced.WrappedComponent = Component.WrappedComponent || Component;
+        Enhanced.displayName = composeNames(enhancerName, getDisplayName(Component));
+        return Enhanced;
+    };
+};
+
+exports.default = hoc;
