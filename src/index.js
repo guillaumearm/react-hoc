b4614fbdbca56b8d96ab78ecc07f432edc91c6cb
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-11-14 21:52:35 +0100

    Add hoc
    customStatics corresponds to the third parameter of hoistNonReactStatics

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+const getDisplayName = WrappedComponent => (
+    WrappedComponent.displayName || WrappedComponent.name || 'Component'
+);
+
+const composeNames = (enhancerName, componentName) => (
+    enhancerName ? `${enhancerName}(${componentName})` : componentName
+);
+
+const hoc = (enhancer, enhancerName, customStatics) => (Component) => {
+    const Enhanced = hoistNonReactStatics(enhancer(Component), Component, customStatics);
+    Enhanced.WrappedComponent = Component.WrappedComponent || Component;
+    Enhanced.displayName = composeNames(enhancerName, getDisplayName(Component));
+    return Enhanced;
+};
+
+export default hoc;
