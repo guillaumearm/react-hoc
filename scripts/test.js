ee8fab6d5ff92146f3ca7043c2f31f4810509150
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-11-16 21:11:46 +0100

    Change scripts and remove dist folder
    Prepare to first test with codeship CI
    - remove dist folder
    - Change scripts in package.json
    - Rename scripts/prepublish.js on test.js
    - Delete scripts/version.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,8 +2,7 @@
 require('shelljs/global');
 const execOrDie = require('./internal/execOrDie');
 
-execOrDie('npm run -s clean-dist', '--- Clean OK ---');
+execOrDie('npm run -s clean', '--- Clean OK ---');
 execOrDie('npm run -s build', '--- Build OK ---');
-execOrDie('npm run -s test', '--- Tests OK ---');
+execOrDie('npm run -s test:coverage', '--- Tests OK ---');
 execOrDie('npm run -s lint', '--- Lint OK ---');
-echo('... Prepublish OK ---');
